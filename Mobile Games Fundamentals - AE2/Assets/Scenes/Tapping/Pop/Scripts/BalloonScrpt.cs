7dac63aeec99e08c015554b8c3ad4149f3f9eb6e
Edward Goodhew <ejgoodhew@btinternet.com>
2018-11-05 00:20:10 +0000

    CountDown Timer Working & count working
    Count Text Doesnt Go Past 10

--- Mobile Games Fundamentals - AE2/Assets/Scenes/Tapping/Pop/Scripts/BalloonScrpt.cs
+++ Mobile Games Fundamentals - AE2/Assets/Scenes/Tapping/Pop/Scripts/BalloonScrpt.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class BalloonScrpt : MonoBehaviour
 {
@@ -8,6 +11,8 @@ public class BalloonScrpt : MonoBehaviour
     public int BalloonsPopped;
     public int PopTarget;
     public bool GameWon;
+    public bool DisableInput;
+    public CountDownScrpt CountDownScrpt;
 
     void Start()
     {
@@ -15,55 +20,101 @@ public class BalloonScrpt : MonoBehaviour
         BalloonsPopped = 0;
         PopTarget = 4;
         GameWon = false;
+        DisableInput = false;
     }
 
     // Update is called once per frame
     void Update()
     {
 #if UNITY_EDITOR
-        if (Input.GetMouseButtonDown(0))
+        if (DisableInput == false)
         {
-            Debug.Log("Click");
-            // Debug to test if if statement works
-            //Converting Mouse Pos to 2D (vector2) World Pos
-            Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
-            RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 100f);
+            StartTimer();
 
-            if (hit && hit.collider.CompareTag("Balloon") == true) // Checking tag of hit sprite
+            if (Input.GetMouseButtonDown(0))
             {
-                Debug.Log("Balloon Hit");
-                Destroy(hit.transform.gameObject); // Destroy gameobject that is hit by raycast
-                BalloonsPopped++;
-            }
-        }
+                Debug.Log("Click");
+                // Debug to test if if statement works
+                //Converting Mouse Pos to 2D (vector2) World Pos
+                Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+                RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 100f);
 
-        if(BalloonsPopped == PopTarget)
-        {
-            GameWon = true;
-            Debug.Log("Game Won");
+                if (hit && hit.collider.CompareTag("Balloon") == true) // Checking tag of hit sprite
+                {
+                    Debug.Log("Balloon Hit");
+                    Destroy(hit.transform.gameObject); // Destroy gameobject that is hit by raycast
+                    BalloonsPopped++;
+                }
+            }
+
+            if (BalloonsPopped >= PopTarget)
+            {
+                GameWon = true;
+                DisableInput = true;
+                Debug.Log("Game Won");
+            }
+
         }
+        else
+        {
+            Debug.Log("Input Disabled");
+        }
+        EndCheck();
+
 
 #elif UNITY_ANDROID
-                if (Input.touchCount == 1)
-        {
-            Debug.Log("Click");
+         if (DisableInput == false)
+         {
+            StartTimer();
 
-            Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
-            RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 100f);
-
-            if (hit && hit.collider.CompareTag("Balloon") == true) // Checking tag of hit sprite
+            if (Input.touchCount == 1)
             {
+                Debug.Log("Click");
+
+                Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+                RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 100f);
+
+                if (hit && hit.collider.CompareTag("Balloon") == true) // Checking tag of hit sprite
+                {
                 Debug.Log("Balloon Hit");
                 Destroy(hit.transform.gameObject); // Destroy gameobject that is hit by raycast
                 BalloonsPopped++;
+                }
             }
-        }
 
-        if(BalloonsPopped == PopTarget)
+            if(BalloonsPopped == PopTarget)
+            {
+                GameWon = true;
+                DisableInput = false;
+                Debug.Log("Game Won");
+            }
+           
+         }
+        else
         {
-            GameWon = true;
-            Debug.Log("Game Won");
+            Debug.Log("Input Disabled");
         }
+         EndCheck();
+
+
 #endif
+    }
+
+    public void EndCheck()
+    {
+        if (CountDownScrpt.TimeUp == true && GameWon == false)
+        {
+            Debug.Log("GameLost");
+            SceneManager.LoadScene(1);
+        }
+        else if (CountDownScrpt.TimeUp == true && GameWon == true)
+        {
+            Debug.Log("Game Won");
+            SceneManager.LoadScene(0);
+        }
+    }
+    public void StartTimer()
+    {
+        CountDownScrpt.StartT();
     }
 }
