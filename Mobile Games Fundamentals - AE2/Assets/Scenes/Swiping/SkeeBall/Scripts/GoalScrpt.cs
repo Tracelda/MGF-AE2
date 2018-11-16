e273e65370d6b76c100a157d8f284fc2451d137c
Edward Goodhew <ejgoodhew@btinternet.com>
2018-11-16 07:28:29 +0000

    SkeeBall Started
    needs testing

---
+++ Mobile Games Fundamentals - AE2/Assets/Scenes/Swiping/SkeeBall/Scripts/GoalScrpt.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalScrpt : MonoBehaviour {
+    public ThrowBall ThrowBall;
+    public bool Hit;
+    private Vector3 Home;
+    public GameObject ReturnSpot;
+    public GameObject Player;
+
+
+	void Start ()
+    {
+        Hit = false;
+        ReturnSpot = GameObject.Find("Home");
+        Home = ReturnSpot.transform.position;
+	}
+	
+
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Ball")
+        {
+            Debug.Log("Goal Scored");
+            ThrowBall.GoalsScored++;
+            Hit = true;
+        }
+    }
+
+    public void Move()
+    {
+        if (Hit == true)
+        {
+            Player.transform.position = new Vector3(Home.x, Home.y, Home.z);
+            Hit = false;
+        }
+    }
+}
---
+++ Mobile Games Fundamentals - AE2/Assets/Scenes/Swiping/SkeeBall/Scripts/ThrowBall.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowBall : MonoBehaviour {
+    public CountDownScrpt CountDownScrpt;
+    public GameManager GameManager;
+    public SwipeInputScrpt SwipeInputScrpt;
+    public GameObject Player;
+    public Rigidbody2D PlayerRigid;
+    public bool GameWon;
+    public bool JumpResult;
+    public int JumpHight;
+    public float RayLength;
+    public int GoalsScored;
+    public int GoalTarget;
+
+    void Start ()
+    {
+        GoalsScored = 0;
+        GoalTarget = 4;
+	}
+	
+
+	void Update ()
+    {
+        SwipeInputScrpt.DetectSwipe();
+        JumpResult = SwipeInputScrpt.DetectSwipe();
+        // Debug.Log(JumpResult);
+        if (JumpResult == true)
+        {
+            Jump();
+        }
+        CheckScore();
+        EndCheck();
+    }
+
+    public void StartTimer()
+    {
+        CountDownScrpt.StartT();
+    }
+
+    public void Jump()
+    {
+        if (Physics2D.Linecast(transform.position, transform.position + new Vector3(0, RayLength, 0), 1 << LayerMask.NameToLayer("Floor")))
+        {
+            PlayerRigid.velocity = new Vector2(PlayerRigid.velocity.y, JumpHight * Time.deltaTime);
+        }
+    }
+
+    public void CheckScore()
+    {
+        if(GoalsScored >= GoalTarget)
+        {
+            GameWon = true;
+        }
+    }
+
+    public void EndCheck()
+    {
+        if (CountDownScrpt.TimeUp == true && GameWon == false)
+        {
+            Debug.Log("GameLost");
+            StaticScrpt.lives--;
+            if (StaticScrpt.lives > 0)
+            {
+                GameManager.LoadNextGame();
+            }
+            else
+            {
+                GameManager.LoadGameOver();
+                Debug.Log("Load Game Over");
+            }
+
+        }
+        else if (CountDownScrpt.TimeUp == true && GameWon == true)
+        {
+            Debug.Log("Game Won");
+            GameManager.LoadNextGame();
+        }
+    }
+}
